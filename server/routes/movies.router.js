42abc6519f0328812e3d93e56ebc306867f3bb4f
Allen Lucke <allenlucke@gmail.com>
2019-12-21 09:29:41 -0600

    added GET route for home page

--- server/routes/movies.router.js
+++ server/routes/movies.router.js
@@ -2,8 +2,21 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool.js')
 
-//GET movies and genres
-router.get('/', (req, res) => {
+//GET movie image, title, and description for home page
+router.get('/', (req,res) => {
+    const queryString = `SELECT "movies".poster, "movies".title, "movies".description FROM "movies"
+    ORDER BY "movies".title;`;
+    pool.query(queryString)
+    .then((response) => {
+        res.send(response.rows);
+    })
+    .catch((err) => {
+        res.sendStatus(500);
+    })
+})
+
+//GET DETAILS: movies titles,description, and genres
+router.get('/details', (req, res) => {
     const queryString = `SELECT "movies".title, "movies".description, "genres".name FROM "movies"
     JOIN "movies_genres" ON "movies".id = "movies_genres".movies_id
     JOIN "genres" ON "movies_genres".genres_id = "genres".id
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Empty Page</p>
+        <h1>Movie Sagas</h1>
       </div>
     );
   }
