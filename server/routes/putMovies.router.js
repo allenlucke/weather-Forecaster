a646510b753ccb137ed17c4669ad31a6f456073a
Allen Lucke <allenlucke@gmail.com>
2019-12-23 09:14:53 -0600

    added a put description to put.movies.router

--- server/routes/putMovies.router.js
+++ server/routes/putMovies.router.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool.js')
 
-//PUT
-router.put('/:id', (req,res) => {
+//PUT to edit description
+router.put('/description/:id', (req,res) => {
     const movieId = req.params.id;
     const changeStatus = req.body.description;
     console.log(movieId, changeStatus);
-    const queryString = `UPDATE "movies" SET "description" = '${changeStatus}' WHERE "id" = $1;`;
+    const queryString = `UPDATE "movies" SET "description" = '${changeStatus}' 
+                        WHERE "id" = $1;`;
     pool.query(queryString, [movieId])
     .then((response) => {
         res.sendStatus(201);
@@ -17,6 +18,21 @@ router.put('/:id', (req,res) => {
         res.sendStatus(500);
     })
 });
-
+//PUT to edit title
+router.put('/title/:id', (req,res) => {
+    const movieId = req.params.id;
+    const changeTitle = req.body.title;
+    console.log(movieId, changeTitle);
+    const queryString = `UPDATE "movies" SET "title" = '${changeTitle}'
+                        WHERE "movies".id = $1;`;
+    pool.query(queryString, [movieId])
+    .then((response) => {
+        res.sendStatus(201);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    })
+});
 
 module.exports = router;
