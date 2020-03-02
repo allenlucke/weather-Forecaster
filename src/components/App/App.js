cfe63d202cae5ba6450d6d472e000d35ecee5748
Allen Lucke <allenlucke@gmail.com>
2020-03-02 14:52:24 -0600

    basic 5 day forecast rendering on page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import './App.css';
+import moment from 'moment';
 //React-Router
 import { HashRouter as Router, Route } from 'react-router-dom';
 
@@ -14,12 +15,37 @@ class App extends Component {
     })
   }
   render() {
-    const forecast = this.props.store.getForecastReducer
+    const forecast = this.props.store.getForecastReducer.map((item, index) => {
+      return(
+          <tbody key={index}>
+              <tr>
+                  <td>{moment(item.Date).format('LL')}</td>
+                  <td>{item.Temperature.Minimum.Value}{item.Temperature.Minimum.Unit}</td>
+                  <td>{item.Temperature.Maximum.Value}{item.Temperature.Maximum.Unit}</td>
+                  <td>{item.Day.PrecipitationIntensity}{item.Day.PrecipitationType}</td>
+                  <td>{item.Night.PrecipitationIntensity}{item.Night.PrecipitationType}</td>
+              </tr>
+          </tbody>
+      )
+  })
     console.log(forecast)
     return (
       <Router>
       <div className="App">
         <h1>Weather Forecast</h1>
+        <h2>Up-To-Date 5 Day Forecast in Kansas City</h2>
+          <table>
+              <thead>
+                  <tr>
+                      <th>Date</th>
+                      <th>Minimum Temperature</th>
+                      <th>MaximumTemperature</th>
+                      <th>Day</th>
+                      <th>Night</th>
+                  </tr>
+              </thead>
+                  {forecast}
+          </table>
       </div>
       </Router>
     );
--- src/redux/sagas/getForecastSaga.js
+++ src/redux/sagas/getForecastSaga.js
@@ -1,16 +1,18 @@
 import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
+// require('dotenv').config();
 
 function* getForecast() {
-    console.log('In getGiphy');
+    console.log('In getForecast');
+    console.log(process.env.API_KEY)
     try {
         const response = yield axios({
             method: 'GET',
-            url: `http://dataservice.accuweather.com/forecasts/v1/daily/1day/64102?apikey=${process.env.API_KEY}`
+            url: `http://dataservice.accuweather.com/forecasts/v1/daily/5day/64102?apikey=zdP9zGcoJn6PG22G20vcNlrUGmbZ1Pgx`
         });
         yield put({
             type: 'SET_FORECAST',
-            payload: response.data
+            payload: response.data.DailyForecasts
         });
     } catch(err) {
         console.log('error fetching forecast', err);
