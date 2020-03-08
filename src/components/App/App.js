a702a8cdb62671ff0a86649f6baba5f1c7f244ee
Allen Lucke <allenlucke@gmail.com>
2020-03-07 22:12:37 -0600

    added ability to change forecast region by inputting zip code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,18 +9,40 @@ import moment from 'moment';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 // import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+
 // import './../../images/AW_RGB_Small';
 
 class App extends Component {
+  state = {
+    zip: 64102
+  }
   // Renders the entire app on the DOM
   componentDidMount() {
     this.props.dispatch({
         type: 'GET_FORECAST_KC'
     })
   }
+  changeField = (event, infoKey) => {
+    this.setState({
+      [infoKey]: event.target.value
+    });
+  };
+
+  changeZip = event => {
+    event.preventDefault();
+      this.props.dispatch({
+        type: 'GET_FORECAST_ZIP',
+        payload: {
+          zip: this.state.zip,
+        }
+      });
+  }
+
   render() {
     const forecast = this.props.store.getForecastReducer.map((item, index) => {
       return(
@@ -29,8 +51,8 @@ class App extends Component {
                   <TableCell>{moment(item.Date).format('dddd')} {moment(item.Date).format('LL')}</TableCell>
                   <TableCell>{item.Temperature.Minimum.Value} {item.Temperature.Minimum.Unit}</TableCell>
                   <TableCell>{item.Temperature.Maximum.Value} {item.Temperature.Maximum.Unit}</TableCell>
-                  <TableCell>{item.Day.PrecipitationIntensity}{item.Day.PrecipitationType}</TableCell>
-                  <TableCell>{item.Night.PrecipitationIntensity}{item.Night.PrecipitationType}</TableCell>
+                  <TableCell>{item.Day.PrecipitationIntensity} {item.Day.PrecipitationType}</TableCell>
+                  <TableCell>{item.Night.PrecipitationIntensity} {item.Night.PrecipitationType}</TableCell>
               </TableRow>
           </TableBody>
       )
@@ -39,7 +61,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Weather Forecast</h1>
-        <h2>Up-To-Date 5 Day Forecast in Kansas City</h2>
+        <h2>Up-To-Date 5 Day Forecast in Kansas City, MO</h2>
           <Table>
               <TableHead className="tableHead">
                   <TableRow>
@@ -52,7 +74,29 @@ class App extends Component {
               </TableHead>
                   {forecast}
           </Table>
-      
+        <div>
+          <h3>Change Location</h3>
+          <h4>Enter Zip Code</h4>
+          <TextField
+              style={{ margin: '13px', flex: '1' }}
+              variant='outlined'
+              size='small'
+              type='number'
+              // className='inputs'
+              label='Zip Code'
+              value={this.state.zip}
+              onChange={event => this.changeField(event, 'zip')}
+              required
+          />
+          <Button
+          style={{ margin: '20px' }}
+          variant='contained'
+          color='primary'
+          onClick={this.changeZip}
+        >
+          Submit
+        </Button>
+        </div>
         <div>
           <img src="images/AW_RGB_Small.jpg" alt="AccuWeather"/>
           
